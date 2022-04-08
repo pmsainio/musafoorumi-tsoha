84d9c74f955d47340832d20e3e853ce8926c7ca4
pmsainio <92957041+pmsainio@users.noreply.github.com>
2022-04-08 14:48:15 +0300

    functioning routes
    routes for front page, artist page, release page and search results

--- routes.py
+++ routes.py
@@ -1,32 +1,43 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 from database import db
+
 
 @app.route("/")
 def index():
-    return render_template("frontpage.html")
+    sql = "SELECT name, performer, year FROM Releases ORDER BY year"
+    releases = db.session.execute(sql)
+    sql = "SELECT performer FROM Releases"
+    artists = db.session.execute(sql)
+    return render_template("frontpage.html", releases=releases, artists=artists)
+
+@app.route("/result", methods=["GET"])
+def result():
+    search = request.args["search"]
+    sql = "SELECT performer, name, year FROM Releases WHERE UPPER(performer) LIKE UPPER(:search) OR UPPER(name) LIKE UPPER(:search) ORDER BY year"
+    result = db.session.execute(sql, {"search":"%"+search+"%"})
+    results = result.fetchall()
+    return render_template("result.html", search=search, results=results)
+
+@app.route("/release/<name>")
+def release(name):
+    sql = "SELECT name, performer FROM releases r WHERE r.name=:name"
+    releases = db.session.execute(sql, {"name":name})
+    sql = "SELECT t.listing, t.name FROM releases r, tracks t WHERE t.release_id = r.id AND r.name=:name"
+    tracks = db.session.execute(sql, {"name":name})
+    sql = "SELECT p.name, p.role FROM releases r, personnel p WHERE p.release_id = r.id AND r.name=:name"
+    personnel = db.session.execute(sql, {"name":name})
+    return render_template("release.html", release=name, releases=releases.fetchall(), tracks=tracks.fetchall(), personnel=personnel.fetchall())
+
+
+@app.route("/artist/<name>")
+def artist(name):
+    sql = "SELECT name from releases WHERE performer=:name ORDER BY year"
+    releases = db.session.execute(sql, {"name":name})
+    return render_template("artist.html", artist=name, releases=releases.fetchall())
 
 @app.route("/contributor/<name>")
 def contr(name):
     sql = "SELECT r.name from releases r, personnel p WHERE p.name=:name and p.release_id IS r.id"
     personnel = db.session.execute(sql, {"name":name})
     return render_template("contributor.html").fetchall
-
-@app.route("/result", methods=["POST"])
-def result():
-    #sql todo
-    return render_template("result.html", name=request.form["search"])
-
-@app.route("/release/<name>")
-def release(name):
-    sql = "SELECT name from releases WHERE name=:name"
-    personnel = db.session.execute(sql, {"name":name})
-    return render_template("contributor.html")
-
-
-@app.route("/artist/<name>")
-def artist(name):
-    sql = "SELECT name from releases WHERE performer=:name"
-    releases = db.session.execute(sql, {"name":name})
-    
-    return render_template("artist.html", artist=name, releases=releases.fetchall())
