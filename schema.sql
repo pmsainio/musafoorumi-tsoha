01481148e792e35b0f9868936ce2d498e90b22b6
pmsainio <92957041+pmsainio@users.noreply.github.com>
2022-04-08 14:49:11 +0300

    schema resets library

--- schema.sql
+++ schema.sql
@@ -1,39 +1,42 @@
+drop TABLE if exists Tracks;
+drop TABLE if exists Personnel;
+drop TABLE if exists Releases;
+
 CREATE TABLE Releases (
     id SERIAL PRIMARY KEY,
     name TEXT,
     genre TEXT,
     performer TEXT,
-    release_year INTEGER
+    year INTEGER
 );
 
 CREATE TABLE Tracks (
-    id SERIAL PRIMARY KEY,
+    listing INTEGER,
     name TEXT,
     release_id INTEGER REFERENCES Releases
 );
 
 CREATE TABLE Personnel (
-    id INTEGER,
     name TEXT,
     role TEXT,
     release_id INTEGER REFERENCES Releases
 );
 
-CREATE TABLE Reviews (
-    id SERIAL PRIMARY KEY,
-    reviewer_id INTEGER REFERENCES Users,
-    riviewee_id INTEGER REFERENCES Releases,
-    score INTEGER
-);
+;--CREATE TABLE Reviews (
+;--    id SERIAL PRIMARY KEY,
+;--   reviewer_id INTEGER REFERENCES Users,
+;--    riviewee_id INTEGER REFERENCES Releases,
+;--    score INTEGER
+;--);
 
-CREATE TABLE Comments (
-    id SERIAL PRIMARY KEY,
-    release_id INTEGER REFERENCES Releases,
-    commenter_id INTEGER REFERENCES Users,
-    timestamp TIMESTAMP);
+;--CREATE TABLE Comments (
+;--    id SERIAL PRIMARY KEY,
+;--    release_id INTEGER REFERENCES Releases,
+;--    commenter_id INTEGER REFERENCES Users,
+;--    timestamp TIMESTAMP);
 
-CREATE TABLE Users (
-    id SERIAL PRIMARY KEY,
-    name TEXT
-    password TEXT
-);
+;--CREATE TABLE Users (
+;--    id SERIAL PRIMARY KEY,
+;--    name TEXT
+;--    password TEXT
+;--);
