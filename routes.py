87e59b2ca7d7e496c34354d6090bd28a3e879d74
pmsainio <92957041+pmsainio@users.noreply.github.com>
2022-04-20 23:39:51 +0300

    login and registration added

--- routes.py
+++ routes.py
@@ -1,8 +1,10 @@
 from app import app
 from flask import render_template, request, redirect, session
 from database import db
+from os import getenv
 from werkzeug.security import check_password_hash, generate_password_hash
 
+app.secret_key = getenv("SECRET_KEY")
 
 
 @app.route("/")
@@ -12,6 +14,53 @@ def index():
     sql = "SELECT performer FROM Releases"
     artists = db.session.execute(sql)
     return render_template("frontpage.html", releases=releases, artists=artists)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        name = request.form["username"]
+        password = request.form["password"]
+        sql = "SELECT name, password FROM users WHERE name=:name"
+        result = db.session.execute(sql, {"name":name})
+        user = result.fetchone()    
+        if not user:
+            return redirect("/")           
+        else:
+            if check_password_hash(user.password, password):
+                session["username"] = name
+                return redirect("/")
+            else:
+                return redirect("/")
+                
+
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
+
+@app.route("/register",methods=["GET", "POST"])
+def signup():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            return redirect("/")
+
+        print("kaikki ok1")
+        hash_value = generate_password_hash(password1)
+        sql = "INSERT INTO Users (name, password) VALUES (:username, :password)"
+        db.session.execute(sql, {"username":username, "password":hash_value})
+        print("kaikki ok2")
+        db.session.commit()
+        session["username"] = username
+        return redirect("/")
+        
 
 @app.route("/result", methods=["GET"])
 def result():
@@ -44,22 +93,5 @@ def contr(name):
     personnel = db.session.execute(sql, {"name":name})
     return render_template("contributor.html", person=name, personnel=personnel.fetchall())
 
-@app.route("/signup",methods=["POST"])
-def signup():
-    hash_value = generate_password_hash(password)
-    sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
-    db.session.execute(sql, {"username":username, "password":hash_value})
-    db.session.commit()
 
 
-@app.route("/login",methods=["POST"])
-def login():
-    username = request.form["username"]
-    password = request.form["password"]
-    session["username"] = username
-    return redirect("/")
-
-@app.route("/logout")
-def logout():
-    del session["username"]
-    return redirect("/")
--- schema.sql
+++ schema.sql
@@ -22,21 +22,21 @@ CREATE TABLE Personnel (
     release_id INTEGER REFERENCES Releases
 );
 
-;--CREATE TABLE Reviews (
-;--    id SERIAL PRIMARY KEY,
-;--   reviewer_id INTEGER REFERENCES Users,
-;--    riviewee_id INTEGER REFERENCES Releases,
-;--    score INTEGER
-;--);
+CREATE TABLE Users (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    password TEXT
+);
 
-;--CREATE TABLE Comments (
-;--    id SERIAL PRIMARY KEY,
-;--    release_id INTEGER REFERENCES Releases,
-;--    commenter_id INTEGER REFERENCES Users,
-;--    timestamp TIMESTAMP);
+CREATE TABLE Reviews (
+    id SERIAL PRIMARY KEY,
+    reviewer_id INTEGER REFERENCES Users,
+    riviewee_id INTEGER REFERENCES Releases,
+    score INTEGER
+);
 
-;--CREATE TABLE Users (
-;--    id SERIAL PRIMARY KEY,
-;--    name TEXT
-;--    password TEXT
-;--);
+CREATE TABLE Comments (
+    id SERIAL PRIMARY KEY,
+    release_id INTEGER REFERENCES Releases,
+    commenter_id INTEGER REFERENCES Users,
+    timestamp TIMESTAMP);
