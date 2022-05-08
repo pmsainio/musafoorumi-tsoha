ff219fe0c980c218a668af98b6dd422592ebec25
Paul <paul.m.sainio@gmail.com>
2022-05-08 15:14:45 +0300

    rounding bug fix

--- releases.py
+++ releases.py
@@ -16,7 +16,7 @@ def load_page(name):
 def load_average(name):
     sql = "SELECT CAST(AVG(a.score) AS FLOAT) FROM Reviews a, Releases b WHERE a.reviewee_id = b.id AND b.name=:name"
     average = db.session.execute(sql, {"name":name})
-    if average == "None":
+    if average == None:
         return average
     average = round(average.fetchone()[0], 2)
     return average
